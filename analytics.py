45c93dfb0e272bc00935e4144957c7482aa1175a
43_50ph14 <so_s@gmx.de>
2023-06-11 20:35:48 +0200

    intermediate state

--- analytics.py
+++ analytics.py
@@ -16,25 +16,26 @@ plt.rcParams["figure.figsize"] = (12, 6)
 #geolocator to map latitude and longitude to city and federal state
 geolocator = Nominatim(user_agent="mapping_coordinates_to_federalstates")
 
-#dic federal states & number of comps in that states [all, 2003-2013, 2014-$rn]
+#dic federal states & number of comps in that states [all, 2003-2014, 2015-$rn]
 federalStaes = {
-    'Schleswig-Holstein' : [0, 0, 0],
-    'Mecklenburg-Vorpommern' : [0, 0, 0],
-    'Hamburg' : [0, 0, 0],
-    'Bremen' : [0, 0, 0],
-    'Niedersachsen' : [0, 0, 0],
-    'Sachsen-Anhalt' : [0, 0, 0],
-    'Brandenburg' : [0, 0, 0],
-    'Berlin' : [0, 0, 0],
-    'Nordrhein-Westfalen' : [0, 0, 0],
-    'Hessen' : [0, 0, 0],
-    'Thüringen' : [0, 0, 0],
-    'Sachsen' : [0, 0, 0],
-    'Rheinland-Pfalz' : [0, 0, 0],
-    'Saarland' : [0, 0, 0],
-    'Baden-Württemberg' : [0, 0, 0],
-    'Bayern' : [0, 0, 0]
+    'Schleswig-Holstein' : 0,
+    'Mecklenburg-Vorpommern' : 0,
+    'Hamburg' : 0,
+    'Bremen' : 0,
+    'Niedersachsen' : 0,
+    'Sachsen-Anhalt' : 0,
+    'Brandenburg' : 0,
+    'Berlin' : 0,
+    'Nordrhein-Westfalen' : 0,
+    'Hessen' : 0,
+    'Thüringen' : 0,
+    'Sachsen' : 0,
+    'Rheinland-Pfalz' : 0,
+    'Saarland' : 0,
+    'Baden-Württemberg' : 0,
+    'Bayern' : 0
 }
+federalStaesE = federalStaesRN = federalStaes
 
 # just a dic to think that something is more efficient because not borthering geolocator all the time
 cities = {'none' : 'nothing'}
@@ -64,7 +65,6 @@ def city_state(city):
 # iterate over comps and fill federalStates dic
 # german comps only
 dfG = Comps[Comps['countryId'] == 'Germany']
-print(dfG.info())
 geocode = partial(geolocator.geocode, language="de")
 
 for index, row in dfG.iterrows():
@@ -78,41 +78,57 @@ for index, row in dfG.iterrows():
             cities[city] = state
         else:
             state = cities[city]
-            
-        federalStaes[state][0] = federalStaes[state][0] + 1
+        
+        federalStaes[state] = federalStaes[state]+ 1
+        # if row['year'] > 2014:#comps after 2014 (2015-$rn)
+        #     federalStaesE[state] = federalStaesE[state] + 1
+        # else:
+        #     federalStaesRN[state] = federalStaesRN[state] + 1
 
 
 
-# generate map
-dfr = gpd.read_file("./GISPORTAL_GISOWNER01_GERMANY_STATES_15.shp")
-rr = pd.DataFrame.from_dict(federalStaes, orient = 'index').reset_index(drop = False)
-print(rr)
-rr = rr.rename(columns = {'index':'StateName1'})
-dfr = dfr.merge(rr, on = 'StateName1')
-print(dfr)
+def data_to_map(dfr, compDis, nr):
+    #generate map
+    rr = pd.DataFrame.from_dict(compDis, orient = 'index').reset_index(drop = False)
+    rr = rr.rename(columns = {'index':'StateName1'})
+    dfr = dfr.merge(rr, on = 'StateName1')
+    rr = rr[0:0]
+    print(type(dfr))
+    print(dfr)
+    return dfr
 
 
-
-karte = copy.copy(cm.hot_r)
-karte = colors.LinearSegmentedColormap.from_list('green', 
+def plot_stuff(title, legend, dfr, plt, ax, idx):
+#joining model outputs to the shapefile
+    karte = copy.copy(cm.hot_r)
+    karte = colors.LinearSegmentedColormap.from_list('green', 
                                         [(0,    '#B3FF80'),
                                          (0.5, '#299617'),
                                          (1,    '#005C29')], N=256)
+    fig.subplots_adjust(top=0.95)
+    fig.suptitle('Distribution of WCA Competitions',fontsize = 15)
+    dfr.plot(column = 0,cmap = karte,ax=ax[idx], legend=False)
+    dfr.geometry.boundary.plot(color=None,edgecolor='k',linewidth = 1,ax=ax[idx]) 
+
+    # iter over data and apply lables
+    for i, row in dfr.iterrows():
+        ax[idx].annotate(text=str(row[0]), xy=row['coords'], horizontalalignment='center')
 
 
-#joining model outputs to the shapefile
-fig, ax = plt.subplots(figsize = (15,12))
-fig.subplots_adjust(top=0.95)
-fig.suptitle('Distribution of WCA Competitions',fontsize = 15)
-dfr.plot(column = 0,cmap = karte,ax=ax, legend=True)
-dfr.geometry.boundary.plot(color=None,edgecolor='k',linewidth = 1,ax=ax) 
 
-#adding labels
+fig, ax = plt.subplots(ncols=2)
+dfr = gpd.read_file("./GISPORTAL_GISOWNER01_GERMANY_STATES_15.shp")
+#adding coords to data structure
 dfr['coords'] = dfr['geometry'].apply(lambda x: x.representative_point().coords[:])
 dfr['coords'] = [coords[0] for coords in dfr['coords']]
+
+
+plot_stuff('title', False, data_to_map(dfr, federalStaes, 0), plt, ax, 0)
+# dfr.drop('0')
 print(dfr)
-for idx, row in dfr.iterrows():
-    plt.annotate(text=str(row[0]), xy=row['coords'], horizontalalignment='center')
+print("scond")
+# plot_stuff('title', False, data_to_map(dfr, federalStaesRN, 1), plt, ax, 1)
+
 
 
 plt.savefig("foo.png")
